4a8da02d92ae19ebc54b0e670bb46549652932c8
Utkarsh <utk2665@gmail.com>
2026-08-18 00:54:46 +0530

    chore: update morrow to 0.1.1

--- Formula/morrow.rb
+++ Formula/morrow.rb
@@ -1,26 +1,26 @@
 class Morrow < Formula
   desc "Calm, keyboard-first terminal workspace for chatting with local LLMs via Ollama"
   homepage "https://github.com/utkarsh125/morrow"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/utkarsh125/morrow/releases/download/v0.1.0/morrow-aarch64-apple-darwin.tar.gz"
-      sha256 "53d53081dd266a75dfe193cd11b30d28b504977373d9223d35293942a4771538"
+      url "https://github.com/utkarsh125/morrow/releases/download/v0.1.1/morrow-aarch64-apple-darwin.tar.gz"
+      sha256 "fc4001547fccd972751d4bb9575234cac5947b76c2f734e97d2b8b586976f4fe"
     else
-      url "https://github.com/utkarsh125/morrow/releases/download/v0.1.0/morrow-x86_64-apple-darwin.tar.gz"
-      sha256 "4a6f1d1451c0966a79294cc33808193af74ab2c6bff0f1d29bf84b635bcdedb2"
+      url "https://github.com/utkarsh125/morrow/releases/download/v0.1.1/morrow-x86_64-apple-darwin.tar.gz"
+      sha256 "3ca06b08e6707cdbfb29a11d4d47773caeea5240826c1589c61622d4aaefff48"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/utkarsh125/morrow/releases/download/v0.1.0/morrow-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "32c88cfdf42be339632d2befb777f64cee5cd57d27e5e518e99459c30506bbc5"
+      url "https://github.com/utkarsh125/morrow/releases/download/v0.1.1/morrow-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "7d720830d6b271d11c90af8acbfe1ee3ff9c390b8cbd18e0edc22d6721f04da6"
     else
-      url "https://github.com/utkarsh125/morrow/releases/download/v0.1.0/morrow-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "d0c13c3af3a6b3306072dcd668592d10bc8b632c769884c73e1249321726ec15"
+      url "https://github.com/utkarsh125/morrow/releases/download/v0.1.1/morrow-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "2075024b549dd19c8f5621a1bb4a06347279a481ba0a9cca82f386b38051c3fc"
     end
   end
 
